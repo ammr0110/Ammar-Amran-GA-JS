b0965c4b62c83ddb3be28dd282ca71ce9d7acf9b
Ammar <73570868+ammr0110@users.noreply.github.com>
2021-08-18 09:05:00 +0800

    Update Objects.js

--- class_01/Objects.js
+++ class_01/Objects.js
@@ -8,7 +8,28 @@
 // Now use an if/else statement to change the output depending on whether you read it yet or not.
 // If you have read it, log a string like 'You have already read "Sapiens", by Yuval Noah Harari'
 // If not, log a string like 'You still need to read "A Life on Our Planet", by David Attenborough.'"
-const books = {};
+const books = [
+  {
+    title: "Sapiens",
+    author: "Yuval Noah Harari",
+    alreadyRead: true,
+  },
+  {
+    title: "A Life on Our Planet",
+    author: "David Attenborough",
+    alreadyRead: false,
+  },
+];
+
+for (let i = 0; i < books.length; i++) {
+  let book = books[i];
+
+  if (book.alreadyRead) {
+    console.log(`You have already read "${book.title}", by ${book.author}.`);
+  } else {
+    console.log(`You still need to read "${book.title}", by ${book.author}.`);
+  }
+}
 
 // The Recipe Card
 // Never forget another recipe!
