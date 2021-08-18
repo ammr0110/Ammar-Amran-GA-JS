8cd16256fa1cf9838de958dc5f2b62ceb0d5e7ec
Ammar <73570868+ammr0110@users.noreply.github.com>
2021-08-18 11:02:52 +0800

    Update Functions.js

--- class_01/Functions.js
+++ class_01/Functions.js
@@ -9,20 +9,33 @@
 //   // ...
 // };
 
-
-
 // Driving Age
 // Write a function that receives a userAge called canDrive
 // If the received age is less than 16, print "Sorry, you can't drive yet"
 // If the received age is equal to or over 16, print "Drive away!"
 // Bonus: If the user can't drive yet, tell them how many years they will have to wait (e.g. "Sorry, you have 4 years to wait until you can drive")
+let userName = prompt("Your name"),
+  userAge = prompt("Your age");
 
+function canDrive(userName, userAge) {
+  let remainYear = 16 - userAge;
+  if (userAge == 15) {
+    console.log(
+      `Sorry ${userName}, you still have a years before you can drive`
+    );
+  } else if (userAge < 16) {
+    console.log(
+      `Sorry ${userName}, you still have ${remainYear} years before you can drive`
+    );
+  } else {
+    console.log("Drive into the sunset!");
+  }
+}
 
+canDrive(userName, userAge);
 
 // The World Translator
 // Write a function called translator that receives a language (e.g. "fr", "eng" etc.). Log out the translated version (e.g. if the language is "eng" - log "Hello World", if the language is "fr" - log "Bonjour le monde" etc.)
-
-
 
 // The Age Calculator
 // Forgot how old you are? Calculate it!
@@ -35,8 +48,6 @@
 // Call the function three times with different sets of values.
 // Bonus: Figure out how to get the current year in JavaScript instead of passing it in.
 
-
-
 // The Lifetime Supply Calculator
 // Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
 
@@ -47,8 +58,6 @@
 // Outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
 // Call that function three times, passing in different values each time.
 // Bonus: Accept floating point values for amountPerDay, and round the result to a round number.
-
-
 
 // The Geometrizer
 // Create 2 functions that calculate properties of a circle, using the definitions here.
@@ -62,8 +71,6 @@
 // Pass the radius to the function.
 // Calculate the area based on the radius, and output "The area is NN".
 
-
-
 // The Temperature Converter
 // It's hot out! Let's make a converter based on the steps here.
 
@@ -75,8 +82,6 @@
 
 // Now store a fahrenheit temperature into a variable.
 // Convert it to celsius and output "NN°F is NN°C".
-
-
 
 // Working with Users
 // Starting with this data...
@@ -116,8 +121,6 @@
 // Bonus: Instead of receiving two strings to illustrate the change (like above), receive an object so you could potentially change multiple things at once. e.g. updateAccount("harpo@ga.co", { password: "gharial", isAdmin: false });
 // Bonus: Make this work only if the current password was also provided as a parameter. e.g. updateAccount("harpo@ga.co", "elephant", { password: "gharial", isAdmin: true });
 // Add a few extra users and make a search function. I'll let you decide on the interface or the function signature
-
-
 
 // A Transit Application
 // Start with this data...
