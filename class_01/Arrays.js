405e5e036417270f5c70df14e5480766b37ce570
Ammar <ammr.0110@gmail.com>
2021-08-21 09:44:10 +0800

    Update Arrays.js

--- class_01/Arrays.js
+++ class_01/Arrays.js
@@ -50,7 +50,11 @@ arrOfArrs[0][0];
 
 // Print "third" by using a dynamic index
 // Bonus: Loop through the second inner array! Print out the first item, the second item, then the third item
-let third = arrOfArrs.find((third) => third.arrOfArrs === "third");
+for (let i = 0; i < arrOfArrs.length; i++) {
+  for (let j = 0; j < arrOfArrs[i].length; j++) {
+    console.log(arrOfArrs[i][j]);
+  }
+}
 
 // Your top choices
 // Create an array to hold your top five choices of something (dogs, books, presidents, whatever you want).
