59a4049a0ed33eedac3feb3569b47cf0e003e38a
Ammar <ammr.0110@gmail.com>
2021-08-22 17:35:43 +0800

    Create The World Translator.js

---
+++ Conditionals/Homework/The World Translator.js
@@ -0,0 +1,18 @@
+// The World Translator
+// Write an if statement that writes Hello World in different languages. Store a language in a variable.
+
+// If the language is "English", log "Hello World"
+// If the language is "French", log "Bonjour le monde"
+// If the language is "Malay", log "Hai dunia" (I think?)
+// Add as many others as you wish!
+let lang = "Malay";
+
+if (lang === "English") {
+  console.log("Hello World");
+} else if (lang === "French") {
+  console.log("Bonjour le monde");
+} else if (lang === "Malay") {
+  console.log("Hai dunia");
+} else {
+  console.log("Perhaps your language is not available at a moment");
+}
