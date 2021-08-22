5de276e57321fb393cdd13aa6b9c8fb677d7ec20
Ammar <ammr.0110@gmail.com>
2021-08-22 17:36:23 +0800

    Create Golf.js

---
+++ Loops/Homework/Golf.js
@@ -0,0 +1,6 @@
+// Golf
+// Using the Golf condition you made before in the conditionals exercise, log out the nickname of every score from 1 to 8 on a par 5 hole
+
+// Bonus: Use a nested for loop to run the same piece of code, but with par changing from 3 to 5
+
+// Bonus: Generate a random par for each iteration
