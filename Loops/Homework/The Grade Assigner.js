88b806a5ed97c105a1adc18997c0166c57763fca
Ammar <ammr.0110@gmail.com>
2021-08-22 21:04:12 +0800

    Update The Grade Assigner.js

--- Loops/Homework/The Grade Assigner.js
+++ Loops/Homework/The Grade Assigner.js
@@ -2,16 +2,18 @@
 // Use the grade assigner code you wrote from the conditionals exercises for every value from 60 to 100 - so your log should show "For 89, you got a B. For 90, you got an A.", etc.
 let testScore = prompt("Enter your test score");
 
-if (testScore <= 100 && testScore >= 80) {
-  console.log("You got an A");
-} else if (testScore < 80 && testScore >= 68) {
-  console.log("You got a B");
-} else if (testScore < 68 && testScore >= 56) {
-  console.log("You got a C");
-} else if (testScore < 56 && testScore >= 40) {
-  console.log("You got a D");
-} else if (testScore < 40 && testScore >= 0) {
-  console.log("You got a F");
-} else {
-  console.log("Please input only a number");
+for (testScore = 60; testScore <= 100; testScore++) {
+  if (testScore <= 100 && testScore >= 80) {
+    console.log(`For ${testScore}, you got an A`);
+  } else if (testScore < 80 && testScore >= 68) {
+    console.log(`For ${testScore}, you got a B`);
+  } else if (testScore < 68 && testScore >= 56) {
+    console.log(`For ${testScore}, you got a C`);
+  } else if (testScore < 56 && testScore >= 40) {
+    console.log(`For ${testScore}, you got a D`);
+  } else if (testScore < 40 && testScore >= 0) {
+    console.log(`For ${testScore}, you got a F`);
+  } else {
+    console.log("Please input only a number");
+  }
 }
