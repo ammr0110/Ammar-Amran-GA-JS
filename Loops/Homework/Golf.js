ffcf6c7cd2192f51f257aedb3296a8738e1ec0f5
Ammar <ammr.0110@gmail.com>
2021-08-22 22:40:49 +0800

    Update Golf.js

--- Loops/Homework/Golf.js
+++ Loops/Homework/Golf.js
@@ -4,3 +4,21 @@
 // Bonus: Use a nested for loop to run the same piece of code, but with par changing from 3 to 5
 
 // Bonus: Generate a random par for each iteration
+let strokes = prompt("How many strokes");
+let par = prompt("How many par");
+
+if (strokes == 1) {
+  console.log("Hole in one");
+} else if (strokes <= par - 2) {
+  console.log("Eagle");
+} else if (strokes == par - 1) {
+  console.log("Birdie");
+} else if (strokes == par) {
+  console.log("Par");
+} else if (strokes == par + 1) {
+  console.log("Bogey");
+} else if (strokes == par + 2) {
+  console.log("Double Bogey");
+} else if (strokes >= par + 3) {
+  console.log("Not sure");
+}
